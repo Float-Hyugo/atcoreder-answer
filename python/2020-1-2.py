f0230ae0a81fcfabf7725a75f82bdd61b4001ac8
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-01-29 10:57:13 +0900

    Update 2020-1-2.py

--- python/2020-1-2.py
+++ python/2020-1-2.py
@@ -1,1 +1,15 @@
+l = input()
+s = list(input())
 
+j = [x for x in s if x == "J"]
+o = [x for x in s if x == "O"]
+i = [x for x in s if x == "I"]
+
+c = [j,o,i]
+x = ""
+
+for a in c:
+  for b in a:
+    x += b
+
+print(x)
