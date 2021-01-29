b92f2f25f43d0bbc66f402cc5df9ee297c87d228
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-01-30 08:18:52 +0900

    Update 2019-1-1.cpp

--- cpp/2019-1-1.cpp
+++ cpp/2019-1-1.cpp
@@ -1,1 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  if(a == b || a == c){
+    cout << a << endl;
+    return 0;
+  }
+  cout << b << endl;
+}
