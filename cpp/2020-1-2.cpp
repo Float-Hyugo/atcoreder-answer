9bb875a481bca8b175eccbbf30a423ae2052ebe0
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-01-29 09:21:46 +0900

    Update 2020-1-2.cpp

--- cpp/2020-1-2.cpp
+++ cpp/2020-1-2.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int x;
+  string s,r;
+  cin >> x >>s;
+  vector<char> c(s.begin(),s.end());
+  for(int l = 0;l < x;l++){
+    if(c.at(l) == 'J'){
+      r += 'J';
+    }
+  }
+  for(int z = 0;z < x;z++){
+    if(c.at(z) == 'O'){
+      r += 'O';
+    }
+  }
+  for(int p = 0;p < x;p++){
+    if(c.at(p) == 'I'){
+      r += 'I';
+    }
+  }
+  cout << r << endl;
+}
+  
+  
