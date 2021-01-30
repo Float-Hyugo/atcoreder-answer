09f82f519bd2b1a52a4cac107304c8a368c29ba9
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-01-30 10:21:11 +0900

    Update 2019-1-3.py

--- python/2019-1-3.py
+++ python/2019-1-3.py
@@ -1,1 +1,24 @@
+c = list()
+input()
+a = map(int,input().split())
+b = map(int,input().split())
 
+while True:
+  if len(a) == 0 and len(b) == 0:
+    breake
+  if len(a) == 0:
+    c.append(b[0])
+    b.pop(0)
+    continue
+  if len(b) == 0:
+    c.append(a[0])
+    a.pop(0)
+    continue
+  if(a[0] <= b[0]):
+    c.append(a[0])
+    a.pop(0)
+    continue
+  c.append(b[0])
+  b.pop(0)
+"\N".join(c)
+print(c)
