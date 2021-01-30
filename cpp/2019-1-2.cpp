61727935b595811a3cdf4b96b14ddc844530f9a8
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-01-30 10:05:00 +0900

    Update 2019-1-2.cpp

--- cpp/2019-1-2.cpp
+++ cpp/2019-1-2.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int n,x;
+  char a;
+  cin >> n;
+  for(int i = 0;i > n;i++){
+    cin >> a;
+    if(a == 'a' || a == 'i' || a == 'u'|| a == 'e' || a == 'o') x++;
+  }
+  cout << x << endl;
+  retur 0;
+}
