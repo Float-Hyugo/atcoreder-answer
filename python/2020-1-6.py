1656d29cda39a19ee6106756e53e86c0c0fd9923
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-02-03 08:49:41 +0900

    Update 2020-1-6.py

--- python/2020-1-6.py
+++ python/2020-1-6.py
@@ -1,1 +1,12 @@
 N = int(input())
+A = list(map(int,input().split()))
+mx = 0
+idx = 0
+for l,v in enumerate(A):
+  if v > mx:
+    mx = v
+    idx = l
+bfr = sum(A[:l])
+after = sum(A[l+1:])
+print(bfr)
+print(after)
