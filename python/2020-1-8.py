1174dab43652374e10695fb4323e013de9c72f51
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-02-03 09:08:25 +0900

    Update 2020-1-8.py

--- python/2020-1-8.py
+++ python/2020-1-8.py
@@ -1,1 +1,14 @@
-
+import sys
+N = int(input())
+A = list(input())
+c = 0
+for i,v in enumerate(A):
+  if i % 2 == 1:
+    if v != 'I':
+      c += 1
+      continue
+  else:
+    if v != 'O':
+      c += 1
+      continue
+print(c)
