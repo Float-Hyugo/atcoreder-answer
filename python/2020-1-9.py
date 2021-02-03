055afc1e159ebf69c3f7f998c9ee67076f0e05fc
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-02-03 09:18:30 +0900

    Update 2020-1-9.py

--- python/2020-1-9.py
+++ python/2020-1-9.py
@@ -1,1 +1,12 @@
-
+N,M map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+A.sort()
+B.sort()
+c = 0
+for x in A:
+  for i,v = enumerate(B):
+    if x < v:
+      c += M - i
+      break
+print(c)
